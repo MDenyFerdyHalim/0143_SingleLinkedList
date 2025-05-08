f2be301e7bd2c4c02aeaf7130892793e70344340
MDenyFerdyHalim <mdenyferdy@gmail.com>
2025-05-08 15:50:49 +0700

    12

--- link list.cpp
+++ link list.cpp
@@ -88,6 +88,20 @@ public:
 
     void traverse()
     {
-        
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
     }
 };
